85a6471144618f50abf99d14579cd4245205c070
Liam Bruno <lbruno@calpoly.edu>
2019-04-29 14:46:25 -0700

    First iteration

---
+++ StarTrackerFilter.py
@@ -0,0 +1,88 @@
+from scipy.integrate import ode
+from AERO_TOOLKIT_NEW import *
+
+class StarTracker_Filter:
+
+	def __init__(self, E_initial, n_initial , w_initial, I_sc, Iwheels, wheel_cant, PROCESS_NOISE, MEASUREMENT_NOISE, COVARIANCE_GUESS):
+
+		self.I_sc = 1.1*I_sc
+		self.Iwheels = Iwheels
+		self.H = identity(7)
+		self.state = hstack([E_initial, n_initial, w_initial])
+		self.P = (COVARIANCE_GUESS**2)*identity(7)
+		self.Q = (PROCESS_NOISE**2)*identity(7)
+		self.R = (MEASUREMENT_NOISE**2)*identity(7)
+		self.wheel_cant = wheel_cant
+
+		solver = ode(propagateEKF)
+		solver.set_integrator('lsoda')
+		solver.set_initial_value(self.state, 0)
+		solver.set_f_params(self.I_sc)
+
+		self.integrator = solver
+
+		hs = []
+		for i in range(4):
+			hs.append(array([cos(wheel_cant)*cos(90*i),
+							 cos(wheel_cant)*cos(90*i),
+							 sin(wheel_cant)]).T)
+		#for
+		self.T = hstack(hs)
+	#constructor
+
+	def calcPhi(self, dt):
+		Ixx = self.I_sc[0][0]
+		Iyy = self.I_sc[1][1]
+		Izz = self.I_sc[2][2]
+
+		eps1 = self.state[0]
+		eps2 = self.state[1]
+		eps3 = self.state[2]
+		eta = self.state[3]
+		w1 = self.state[4]
+		w2 = self.state[5]
+		w3 = self.state[6]
+
+		F = array([[0,w3/2,-w2/2,w1/2,eta/2,-eps3/2,eps2/2],
+				   [-w3/2,0,w1/2,w2/2,eps3/2,eta/2,-eps1/2],
+				   [w2/2,-w1/2,0,w3/2,-eps2/2,eps1/2,eta/2],
+			       [-w1/2,-w2/2,-w3/2,0,-eps1/2,-eps2/2,-eps3/2],
+			       [0,0,0,0,0,(w3*(Iyy-Izz))/Ixx,(w2*(Iyy-Izz))/Ixx],
+			       [0,0,0,0,-(w3*(Ixx-Izz))/Iyy,0,-(w1*(Ixx-Izz))/Iyy],
+			       [0,0,0,0,(w2*(Ixx-Iyy))/Izz,(w1*(Ixx-Iyy))/Izz,0]])
+
+		return F*dt + identity(7)
+	#getF
+
+	def predict_with_ode(self, dt):
+		self.integrator.integrate(self.integrator.t + dt)
+		return self.integrator.y
+	#predict_with_ode
+
+	def predict_euler(self, dt):
+		E = self.state[0:3]
+		n = self.state[3]
+		w = self.state[4:7]
+		dE = .5 * (n * identity(3) + crux(E)) @ w
+		dn = -.5 * dot(E, w)
+		dw = inv(self.I_sc) @ (-crux(w) @ (self.I_sc @ w))
+		dstate = hstack([dE, dn, dw])
+		return self.state + dstate*dt
+	#predict_euler
+
+	def update(self, z, dt):
+		Phi = self.calcPhi(dt)
+		x_prediction = self.predict_euler(dt)
+		P_prediction = Phi@self.P@Phi.T + self.Q
+		y = z - self.H@x_prediction
+		S = self.R + self.H@self.P@self.H.T
+		K = P_prediction@self.H.T@inv(S)
+		self.state = x_prediction + K@y
+		self.P = (identity(7) - K@self.H)@P_prediction
+		return self.state
+	#update
+
+	def getState(self):
+		return self.state
+	#getState
+# starTrackerFilter
---
+++ test.py
@@ -0,0 +1,131 @@
+from StarTrackerFilter import *
+from AERO_TOOLKIT_NEW import *
+import matplotlib.pyplot as plt
+
+def main():
+
+    n = 1
+    E = array([0, 0, 0])
+    w = array([.1, 0.07, -0.04])
+    w_wheels = zeros(4)
+
+    # SC Properties:
+    L = 3 # [m]
+    W = 3 # [m]
+    H = 5 # [m]
+
+    m = 500 # [kg]
+
+    I = array([[(1/12)*m*(L**2 + W**2), 0, 0],[0, (1/12)*m*(L**2 + H**2), 0],[0, 0, (1/12)*m*(H**2 + W**2)]])
+    Iw = 0.5 # Inertia of wheels around spin axis [kg*m^2]
+    wheel_cant = pi/8 # [rad]
+
+    # Noise estimates (standard deviations) for EKF:
+    PROCESS_NOISE = sqrt(1e-10)
+    MEASUREMENT_NOISE = sqrt(1e-3)
+    COVARIANCE_GUESS = sqrt(1e-3)
+    STAR_TRACKER_NOISE = sqrt(1e-3)
+    GYRO_NOISE = sqrt(1e-3)
+
+    aguess = array([1,2,3])/norm(array([1,2,3]))
+    thetaguess = 0.1
+    Eguess = aguess*sin(thetaguess/2)
+    nguess = cos(thetaguess/2)
+    w_guess = array([0.5, 0.5, 0.5])
+
+    EKF = StarTracker_Filter(Eguess, nguess, w_guess,  I, Iw, wheel_cant, PROCESS_NOISE, MEASUREMENT_NOISE, COVARIANCE_GUESS)
+    state = hstack([E, n, w, w_wheels])
+    
+    solver = ode(propagateTruth)
+    solver.set_integrator('dopri5')
+    solver.set_initial_value(state, 0)
+    solver.set_f_params(I)
+    
+    tspan = 1000 # Total simulation time [sec]
+    dt = .1
+    t = [] # [sec]
+    newstate = []
+    measurements = []
+    state_estimate = []
+    ang_vel_error = []
+
+    newstate.append(solver.y)
+    t.append(solver.t)
+    state_estimate.append(EKF.getState())
+
+    while solver.successful() and (solver.t < tspan):
+        q_measurement = solver.y[0:4] + random.normal(0, STAR_TRACKER_NOISE, (4,))
+        q_measurement = q_measurement/norm(q_measurement)
+        w_measurement = solver.y[4:7] + random.normal(0, GYRO_NOISE, (3,))
+        measurement = hstack([q_measurement, w_measurement])
+        measurements.append(measurement)
+        estimate = EKF.update(measurement, dt)
+        ang_vel_error.append(norm(estimate[4:7]) - norm(solver.y[4:7]))
+        state_estimate.append(estimate)
+        solver.integrate(solver.t + dt)
+        t.append(solver.t)
+        newstate.append(solver.y)
+    #while
+
+    t = hstack(t)
+    newstate = vstack(newstate)
+    state_estimate = vstack(state_estimate)
+
+    fig, axes1 = plt.subplots(4, 1, squeeze = False)
+
+    axes1[0][0].plot(t, newstate[:,0])
+    axes1[0][0].plot(t, state_estimate[:,0], '.')
+    axes1[0][0].set_title('eps1')
+    axes1[0][0].legend(['Truth', 'Estimate'])
+
+    axes1[1][0].plot(t, newstate[:,1])
+    axes1[1][0].plot(t, state_estimate[:,1], '.')
+    axes1[1][0].set_title('eps2')
+
+    axes1[2][0].plot(t, newstate[:,2])
+    axes1[2][0].plot(t, state_estimate[:,2], '.')
+    axes1[2][0].set_title('eps3')
+
+    axes1[3][0].plot(t, newstate[:,3])
+    axes1[3][0].plot(t, state_estimate[:,3], '.')
+    axes1[3][0].set_title('eta')
+
+    fig = plt.figure()
+
+    w1_error = state_estimate[:,4] - newstate[:, 4]
+    w2_error = state_estimate[:, 5] - newstate[:, 5]
+    w3_error = state_estimate[:, 6] - newstate[:, 6]
+
+    plt.plot(t, w1_error)
+    plt.plot(t, w2_error)
+    plt.plot(t, w3_error)
+    plt.grid()
+    plt.title('Angular Velocity Error [rad/s]')
+    plt.legend(['Body X', 'Body Y', 'Body Z'])
+
+    plt.show()
+#main
+    
+def propagateTruth(t, state, I):
+    E = state[0:3]
+    n = state[3]
+    w = state[4:7]
+    
+    dE = .5*(n*identity(3) + crux(E))@w
+    dn = -.5*dot(E, w)
+
+    Td = random.normal(0, 1e-3, (3,))
+
+    dw = inv(I) @ (Td - crux(w)@ (I@w))
+    
+    wwdot = zeros(4)
+    
+    return hstack([dE, dn, dw, wwdot])
+#propagateTruth
+
+if __name__ == "__main__":
+    main()
+#if
+
+
+
